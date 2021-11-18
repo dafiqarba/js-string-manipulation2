af651cb3743f485094790d4d6ecc1a7fcf540917
dafiqarba <daviqarba@live.com>
2021-11-18 19:47:48 +0700

    add comments for variables and function block description

--- js/script.js
+++ js/script.js
@@ -1,86 +1,86 @@
+// Enable ES6 strict mode
 "use strict";
 
+// Button element
 let button = document.querySelector(".input-button");
+// Output elements
 let displayOutput1 = document.querySelector(".out1");
 let displayOutput2 = document.querySelector(".out2");
 let displayOutput3 = document.querySelector(".out3");
 let displayOutput4 = document.querySelector(".out4");
+// Radio elements
+let radioFibo = document.querySelector("#fibonacci");
+let radioOdd = document.querySelector("#ganjil");
+let radioEven = document.querySelector("#genap");
+let radioNum = document.querySelector("#angka");
 
+// Click button response
 button.addEventListener("click", () => {
+  // Store user input string
   let userInput = document.querySelector(".input-text").value;
-
-  let radioFibo = document.querySelector("#fibonacci");
-  let radioOdd = document.querySelector("#ganjil");
-  let radioEven = document.querySelector("#genap");
-  let radioNum = document.querySelector("#angka");
-
+  // Store all sequence as key and value pair in a single object
   let resultOfSequence = {};
+  // Store type of sequence (value will be updated when a radio button is selected)
   let typeOfSequence;
-
+  // Check which radio button is selected, then execute the replaceVowel function, and display output
   if (radioFibo.checked) {
     typeOfSequence = document.querySelector("#fibonacci").value;
-    displayOutput1.textContent = replaceVowel(
-      userInput,
-      generateFibonacci(userInput),
-      typeOfSequence
-    );
+    displayOutput1.textContent = replaceVowel(userInput, typeOfSequence, generateFibonacci(userInput));
   } else if (radioOdd.checked) {
     typeOfSequence = document.querySelector("#ganjil").value;
-    displayOutput2.textContent = replaceVowel(userInput, generateOdd(userInput), typeOfSequence);
+    displayOutput2.textContent = replaceVowel(userInput, typeOfSequence, generateOdd(userInput));
   } else if (radioEven.checked) {
     typeOfSequence = document.querySelector("#genap").value;
-    displayOutput3.textContent = replaceVowel(userInput, generateEven(userInput), typeOfSequence);
+    displayOutput3.textContent = replaceVowel(userInput, typeOfSequence, generateEven(userInput));
   } else if (radioNum.checked) {
     typeOfSequence = document.querySelector("#angka").value;
     displayOutput4.textContent = replaceVowel(
       userInput,
-      generateNumber(userInput),
       typeOfSequence,
+      generateNumber(userInput),
       generateAlphabet(userInput)
     );
   }
-
+  // Print object to console
   console.log(resultOfSequence);
 
-  function checkChar(string) {
-    return string.trim().split(" ").join("").length;
-  }
-
+  // This function will check total vowel of a string and returns the total vowel value to the function call
   function checkTotalVowels(string) {
     let vowels = "aiueoAIUEO";
-    let count = 0;
+    let totalVowel = 0;
 
     for (let i = 0; i < string.length; i++) {
       if (vowels.includes(string[i])) {
-        count++;
+        totalVowel++;
       }
     }
-    return count;
+    return totalVowel;
   }
 
+  // This function will generate a sequence of fibonacci
   function generateFibonacci(string) {
     let fibonacciNum = [0];
     let previousNumber = 1;
     let presentNumber = 0;
     let sum = 0;
-    let vowelCount = checkTotalVowels(string);
+    let totalVowel = checkTotalVowels(string);
 
-    for (let i = 0; i <= vowelCount - 2; i++) {
+    for (let i = 0; i <= totalVowel - 2; i++) {
       sum = previousNumber + presentNumber;
       previousNumber = presentNumber;
       presentNumber = sum;
       fibonacciNum.push(sum);
     }
-    // resultOfSequence.push({ fibonacciSequence: fibonacciNum });
     resultOfSequence.fibonacciSequence = fibonacciNum;
     return resultOfSequence;
   }
 
+  // This function will generate a sequence of even number
   function generateEven(string) {
-    let vowelCount = checkTotalVowels(string);
+    let totalVowel = checkTotalVowels(string);
     let evenNumber = [];
 
-    for (let i = 2; i <= 2 * vowelCount; i++) {
+    for (let i = 2; i <= 2 * totalVowel; i++) {
       if (i % 2 == 0) {
         evenNumber.push(i);
       }
@@ -89,11 +89,12 @@ button.addEventListener("click", () => {
     return resultOfSequence;
   }
 
+  // This function will generate a sequence of odd number
   function generateOdd(string) {
-    let vowelCount = checkTotalVowels(string);
+    let totalVowel = checkTotalVowels(string);
     let oddNumber = [];
 
-    for (let i = 1; i <= 2 * vowelCount; i++) {
+    for (let i = 1; i <= 2 * totalVowel; i++) {
       if (i % 2 != 0) {
         oddNumber.push(i);
       }
@@ -102,56 +103,35 @@ button.addEventListener("click", () => {
     return resultOfSequence;
   }
 
+  // This function will generate a sequence of number
   function generateNumber(string) {
-    let totalChar = checkTotalVowels(string) / 2;
+    let totalVowel = checkTotalVowels(string) / 2;
     let numSequence = [];
 
-    for (let i = 1; i <= totalChar; i++) {
-      numSequence.push(i);
+    for (let i = 0; i <= totalVowel; i++) {
+      numSequence.push(i + 1);
     }
     resultOfSequence.numSequence = numSequence;
     return resultOfSequence;
   }
+
+  // This function will generate a sequence of alphabet
   function generateAlphabet(string) {
-    let alphabet = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
     ];
     let alpSequence = [];
-    let totalChar = checkTotalVowels(string) / 2;
+    let totalVowel = checkTotalVowels(string) / 2;
 
-    for (let i = 0; i <= totalChar - 1; i++) {
+    for (let i = 0; i <= totalVowel - 1; i++) {
       alpSequence.push(alphabet[i]);
     }
     resultOfSequence.alpSequence = alpSequence;
     return resultOfSequence;
   }
 
-  function replaceVowel(string, sequence, typeOfSequence, sequenceAlph) {
+  // This function will replace vowel letters of a string based on selected sequence
+  function replaceVowel(string, typeOfSequence, sequence, sequenceAlph) {
     let vowels = "aiueoAIUEO";
     let arrayOfString = string.split("");
     let count = 0;
@@ -183,28 +163,13 @@ button.addEventListener("click", () => {
         }
       }
     }
-
     return arrayOfString.join("");
   }
 });
-/*
-
-buat function untuk menampung bilangan dibawah sepanjang n
-1. fibonacci
-2. ganjil
-3. genap
-4. angka huruf (1a2b3c...)
-
-simpan hasilnya dalam sebuah objeck dan akan digunakan untuk mengisi kalimat pada tugas sebelumnya
-
-tugas sebelumnya huruf vokal diganti dengan fibonacci
-tugas kali ini huruf vokal diganti denga ke 4 deret diatas.
-
-user bisa memilih, misal menggunakan radio button.. deret mana yg akan menggantikan huruf fokal
-
-*/
 
 /* 
+  This function is able to insert sequence of number before each letter of a string
+
   function replaceNumber(string, sequence, typeOfSequence) {
     let arrayOfString = string.split("");
     let numCount = 0;
